9d6e4aab67720bd829018514831cd33e44cc5dc0
Tianying Xia <xiaty1@gmail.com>
2021-09-03 17:10:51 -0600

    curl Step 10 Modify your Python script...

--- lab1script.py
+++ lab1script.py
@@ -4,3 +4,8 @@ print(requests.__version__)
 
 # GET the Google homepage 
 page = requests.get('http://www.google.com/')
+#print('\n', page.text)
+
+# "Modify your Python script so that it downloads itself from GitHub and prints out its own source code from GitHub." 
+rawPg = requests.get('https://raw.githubusercontent.com/csportat/CMPUT404_lab1/main/lab1script.py')
+print('\n', rawPg.text)
